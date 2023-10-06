4a394625e604b8781aa2285045d02366f6caae9f
Jake Lipscomb <jlipscombtx95@gmail.com>
2023-10-06 11:41:37 -0500

    updated package.json, README, and server.js. established a proper connection in server.js to run command-line prompts.

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # employee_tracker
+  by Jake Lipscomb
+  ![License](https://img.shields.io/badge/License-MIT-blue.svg)
+
+## Click the screenshot to watch my walkthrough video on YouTube!
+[![Watch the video]()]()
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Requirements](#requirements)
+  * [Usage](#usage)
+  * [Contact-Info](#contact-info)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+* [License](#license)
+
+## Description
+The purpose of this project was to create a database that can be used to track employees, their roles, and their departments. This application is run through the command line and uses MySQL to store and retrieve data. The user can add, view, and update employees, roles, and departments. The user can also view employees by manager and view the total utilized budget of a department. 
+## Requirements
+    inquirer: "^8.2.4",
+    mysql2: "^2.2.5"
+
+## Usage
+Node, JSON, JavaScript
+## Contact-Info
+* Username: jakelipscomb (https://github.com/jakelipscomb/)
+* Email: jlipscombtx95@gmail.com
+## Contributors
+none
+## Testing
+
+To recreate this project, begin by cloning through:
+
+    git clone https://github.com/jakelipscomb/employee_tracker.git
+
+To install required packages, run in terminal:
+
+    npm i
+
+Log into MySQL and run the following commands:
+
+    source db/schema.sql;
+    source db/seeds.sql;
+
+Quit MySQL
+
+Run application with
+
+    node server.js
+
+This will run the application locally within the `command line`.
+
+
+## License
+      This Project is using the MIT license.
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import mysql from 'mysql2';
-import inquirer from 'inquirer';
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 const db = mysql.createConnection (
     {
@@ -12,17 +12,21 @@ const db = mysql.createConnection (
 
 db.connect(err => {
     if (err) throw err;
-    else {
+    console.log('Connected to the company database.');
+    start();    
+});
+
+start = () => {
         console.log("********************")
         console.log("*                  *")
         console.log("* Employee Tracker *")
         console.log("*                  *")
         console.log("********************")
-    }
-})
-console.log('Connecting to the company db...')
+        promptUser();
+    };
 
-const choices = [
+const promptUser = () => {
+    inquirer.prompt([
         {
             type: 'list',
             name: 'choices',
@@ -33,20 +37,14 @@ const choices = [
                 {key: 'view all employees', value: 'ALL_EMPLOYEES'},
                 {key: 'add a role', value: 'ADD_A_ROLE'},
                 {key: 'add a department', value: 'ADD_A_DEPARTMENT'},
-                {key: 'add a department', value: 'ADD_AN_EMPLOYEE'},
-                {key: 'update an employee role', value: 'UPDATE_EMPLOYEE_ROLE'}
+                {key: 'add an employee', value: 'ADD_AN_EMPLOYEE'},
+                {key: 'update an employee role', value: 'UPDATE_EMPLOYEE_ROLE'},
+                {key: 'view employees by department', value: 'VIEW_EMPLOYEES_BY_DEPARTMENT'},
+                {key: 'view employees by manager', value: 'VIEW_EMPLOYEES_BY_MANAGER'},
+                {key: 'delete a department', value: 'DELETE_A_DEPARTMENT'},
+                {key: 'delete a role', value: 'DELETE_A_ROLE'},
+                {key: 'delete an employee', value: 'DELETE_AN_EMPLOYEE'},
             ]
         }
-]
-
-function init() {
-    inquirer.prompt(choices)
-    .then((res) => {
-        console.log(res.key);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-}
-
-init();
+    ])
+};
